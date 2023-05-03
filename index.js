8b094759d0760a35b1d4ddad41ef3afe2b54d203
shejanNizam <nizam.shejan11@gmail.com>
2023-05-03 12:35:55 +0600

    url created

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 7000;
 
+const chefData = require("./data/chefData.json");
 // middle ware
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("chef recipe hunter is running");
+});
+
+app.get("/chef_data", (req, res) => {
+  res.send(chefData);
 });
 
 app.listen(port, () => {
